c7b6cbdcfac529d99d1a1099f13898b43d3008bd
masslany <p.atryk@outlook.com>
2019-10-19 14:21:54 +0200

    register page added

--- ProjektZespolowy/urls.py
+++ ProjektZespolowy/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import (
+    home_page_view,
+    register_view
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page_view),
+    path('register/', register_view),
 ]
---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home_page_view(request):
+    return render(request, "home_page.html")
+
+
+def register_view(request):
+    return render(request, "register.html")
