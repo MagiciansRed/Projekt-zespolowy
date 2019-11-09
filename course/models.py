cde6890292bc9e6d493bc59057bb3feefefa4327
masslany <p.atryk@outlook.com>
2019-11-09 21:11:30 +0100

    further model implementation

--- course/models.py
+++ course/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from account.models import Account
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -8,6 +10,7 @@ class Course(models.Model):
     name = models.CharField(max_length=60, null=False, blank=False)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True, verbose_name="Date added")
+    slug = models.SlugField(blank=True, unique=True)
 
 
 class Subscription(models.Model):
@@ -24,7 +27,13 @@ class Word(models.Model):
 class WordDetails(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    value = models.IntegerField()
-    is_learnt = models.BooleanField()
+    value = models.IntegerField(default=0, null=True)
+    is_learnt = models.BooleanField(null=True, default=False)
 
 
+def pre_save_course_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.author.username + "-" + instance.name)
+
+
+pre_save.connect(pre_save_course_receiver, sender=Course)
