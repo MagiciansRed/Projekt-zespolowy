f8b2f73b2e8d4ee4a82376a4898b722e5768ff4c
masslany <p.atryk@outlook.com>
2019-11-22 21:14:51 +0100

    initial subscription added

--- course/views.py
+++ course/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from course.models import Course
+from course.models import Course, Subscription
+
 
 # Create your views here.
-
 
 
 def course_list_view(request):
@@ -17,5 +17,23 @@ def detail_course_view(request, slug):
     context = {}
     course = get_object_or_404(Course, slug=slug)
     context['course_detail'] = course
+
+    user = request.user
+    sub = Subscription()
+    entry = Subscription.objects.filter(course=course)
+
+    if entry.exists():
+        for e in entry:
+            if e.user.__eq__(user):
+                print("user exists")
+                context['subscription_state'] = True
+                break
+    else:
+        context['subscription_state'] = False
+        if request.POST.get('subscribe'):
+            sub.course = course
+            sub.user = user
+            sub.save()
+            context['subscription_state'] = True
 
     return render(request, 'course/detail_course.html', context)
