ec61845da36e2016286f8cf09f3c7bb9c703af2a
masslany <p.atryk@outlook.com>
2019-11-30 14:57:07 +0100

    refined dashboard layout

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from course.models import Course, Subscription
 
 
 # Create your views here.
 def home_page_view(request):
+    context = {}
     user = request.user
     if user.is_authenticated:
-        return render(request, "dashboard.html")
+        subscribed_courses = Subscription.objects.filter(user=user)
+        context['subscriptions'] = subscribed_courses
+        context['profile'] = user
+        return render(request, "dashboard.html", context)
     else:
         return render(request, "home_page.html")
 
