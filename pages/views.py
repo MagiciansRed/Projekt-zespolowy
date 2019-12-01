d16afb7c901ff8480cea3ad1c81d02dc80874c3f
masslany <p.atryk@outlook.com>
2019-12-01 12:41:10 +0100

    added words learned counter to dashboard

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from course.models import Course, Subscription
+from course.models import Course, Subscription, WordDetails
 
 
 # Create your views here.
@@ -8,8 +8,11 @@ def home_page_view(request):
     user = request.user
     if user.is_authenticated:
         subscribed_courses = Subscription.objects.filter(user=user)
+        words = WordDetails.objects.all().filter(user=user, is_learnt=True)
+        number_words_learned = len(words)
         context['subscriptions'] = subscribed_courses
         context['profile'] = user
+        context['words_learned'] = number_words_learned
         return render(request, "dashboard.html", context)
     else:
         return render(request, "home_page.html")
